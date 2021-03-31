c260599900e7c192e18758783c6969540f2fb369
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-31 12:33:17 +0800

    Setup mailtrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILTRAP_USERNAME'],
+  :password => ENV['MAILTRAP_PASSWORD'],
+  :address => 'smtp.mailtrap.io',
+  :domain => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = true  
+  config.action_mailer.default_url_options = { :host => 'ror-training-saas-app.herokuapp.com', :protocol => 'https' }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
