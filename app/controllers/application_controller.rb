ba0045dddcaeb34730eef0df3861f1addb72905c
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-04-06 14:58:49 +0800

    Add tenant helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_tenant, :private_tenant?
+
+  def current_tenant
+    Apartment::Tenant.current
+  end
+
+  def private_tenant?
+    !(current_tenant == 'development' || current_tenant == 'production')
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :projects
   devise_for :users
   root 'welcome#index'
+  get '/projects' => 'project#index', :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
