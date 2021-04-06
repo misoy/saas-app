2aca64f7bb0628e8fc1edf3d3804d502d070b5c7
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-04-06 11:17:15 +0800

    Add confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_095003) do
+ActiveRecord::Schema.define(version: 2021_04_06_031045) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 2021_03_31_095003) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "subdomain"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
